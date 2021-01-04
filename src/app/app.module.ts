12e4ea579dcb533859c901bfceb75e148fc7b69b
dymafr <erwan1309@hotmail.com>
2021-01-04 16:55:20 +0100

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
-import { NgModule } from "@angular/core";
+import { LOCALE_ID, NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
-
 import { AppComponent } from "./app.component";
+import localeFr from "@angular/common/locales/fr";
+import { registerLocaleData } from "@angular/common";
+registerLocaleData(localeFr);
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
   declarations: [AppComponent],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [{ provide: LOCALE_ID, useValue: "fr-FR" }]
 })
 export class AppModule {}
